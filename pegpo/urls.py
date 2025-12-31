78a27a91b76919af865c453256f781f4269efea8
widecity <amal@bigburry.com>
2025-12-31 13:31:42 +0530

    Update urls.py

--- pegpo/urls.py
+++ pegpo/urls.py
@@ -1,7 +1,8 @@
 # pegpo/urls.py
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     # Root UI (so http://127.0.0.1:8000/ serves your chat)
     path("", include("ui.urls")),
@@ -11,3 +12,5 @@ urlpatterns = [
 
     path("admin/", admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
